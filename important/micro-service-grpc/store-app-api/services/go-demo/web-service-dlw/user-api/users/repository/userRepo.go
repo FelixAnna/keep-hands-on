4e6ec0d47a0919211950b38f63afb1f8f3285109
Felix Yu <Felix_Yu@epam.com>
2021-11-17 17:30:44 +0800

    user-api refactor

---
+++ important/micro-service-grpc/store-app-api/services/go-demo/web-service-dlw/user-api/users/repository/userRepo.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+type UserRepo struct {
+}
+
+func (u *UserRepo) Initial() {
+	// Using the SDK's default configuration, loading additional config
+	// and credentials values from the environment variables, shared
+	// credentials, and shared configuration files
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-1"))
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	// Using the Config value, create the DynamoDB client
+	svc := dynamodb.NewFromConfig(cfg)
+
+	// Build the request with its input parameters
+	resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5),
+	})
+	if err != nil {
+		log.Fatalf("failed to list tables, %v", err)
+	}
+
+	//svc.GetItem()
+
+	fmt.Println("Tables:")
+
+	for _, tableName := range resp.TableNames {
+		fmt.Println(tableName)
+	}
+}
