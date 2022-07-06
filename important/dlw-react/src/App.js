bb43d9aa14a41cb00b909254f73e4c1c6f6141e4
FelixAnna <yuecnu@hotmail.com>
2022-07-06 18:44:45 +0800

    initial react app for math service

---
+++ important/dlw-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import SearchBar from './component/SearchBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <SearchBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
