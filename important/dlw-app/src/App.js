e18fdfd8b8e7f8572015aa245d6a33201c33154f
FelixAnna <yuecnu@hotmail.com>
2022-07-15 18:04:56 +0800

    base work for router

--- important/dlw-app/src/App.js
+++ important/dlw-app/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import Mathematicals from './features/mathematicals/index';
+import Login from './features/login';
 
 function App() {
   return (
     <div className="App">
-      <Mathematicals />
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="math" element={<Mathematicals />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
