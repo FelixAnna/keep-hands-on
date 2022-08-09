7d7ce93496903753645f9016e1d09db86bbf56ff
FelixAnna <yuecnu@hotmail.com>
2022-08-09 12:38:41 +0800

    enable auto login when refresh

--- important/dlw-app/src/features/login/index.js
+++ important/dlw-app/src/features/login/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  useNavigate,
+  useLocation,
+} from 'react-router-dom';
 import Link from '@mui/material/Link';
 import getAuthorizeUrl, { githubAuthOptions } from './provider';
+import { currentLoginStatus } from './reducer';
 
 function Login() {
   const st = 'state123';
@@ -10,6 +15,14 @@ function Login() {
   const location = useLocation();
   const from = location.state?.from?.pathname || '/';
   sessionStorage.setItem('redirect_url', from);
+
+  const navigate = useNavigate();
+  const loginStatus = useSelector(currentLoginStatus);
+  useEffect(() => {
+    if (loginStatus) {
+      navigate(from);
+    }
+  }, [loginStatus]);
 
   return (
     <div>
