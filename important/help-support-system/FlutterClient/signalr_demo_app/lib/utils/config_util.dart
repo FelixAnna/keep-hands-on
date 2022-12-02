b7ec09a54ad66a335de685b5ebfa4da6b3b4010e
felixanna <yuecnu@hotmail.com>
2022-12-02 23:10:53 +0800

    remove duplicate files

--- important/help-support-system/FlutterClient/signalr_demo_app/lib/utils/config_util.dart
+++
@@ -1,9 +0,0 @@
-import "package:flutter/services.dart" as s;
-import 'package:yaml/yaml.dart';
-
-Future<String> loadConfig(section) async {
-  final yamlString = await s.rootBundle.loadString('assets/config.yaml');
-  final dynamic yamlMap = loadYaml(yamlString);
-
-  return yamlMap[section];
-}
